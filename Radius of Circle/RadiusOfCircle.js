ebe26a12db273f90aee6394e7c9d4db3a2e5b5eb
ghasemizade <hosein@ghasemizade.com>
2022-07-08 23:56:54 +0430

    calculate diameter, environment, area of circle

---
+++ Radius of Circle/RadiusOfCircle.js
@@ -0,0 +1,11 @@
+var radius = prompt("Enter radius of circle :")
+var PI = 3.14
+
+
+var diameter = Number(radius) * 2;
+var environment = Number(radius)* 2 * PI;
+var area = Number(radius)* Number(radius) * PI;
+
+alert("diameter :"+ diameter)
+alert("environment :"+environment)
+alert("area :" +area)

606de92d9b6a6d9eb125cada97e0a8d688c2673b
ghasemizade <hosein@ghasemizade.com>
2022-07-09 09:44:59 +0430

    power of 1,2,3;

---
+++ Pow 1,2,3/Pow1,2,3.js
@@ -0,0 +1,11 @@
+var Input = prompt("Enter the number :")
+
+var test = Number(Input)
+
+var pow1 = test;
+var pow2 = test * test;
+var pow3 = test * test * test;
+
+alert("power of 1 :" + pow1)
+alert("power of 2 :" + pow2)
+alert("power of 3 :" + pow3)

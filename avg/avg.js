dd5ec12a3d0f2848f4826d0dfba345e0b695a312
ghasemizade <hosein@ghasemizade.com>
2022-07-09 11:21:37 +0430

    input four number and then calculate average of numbers

---
+++ avg/avg.js
@@ -0,0 +1,14 @@
+var input1 = prompt("Enter first number")  // Create a Scanner object
+var input2 = prompt("Enter second number")  // Create a Scanner object
+var input3 = prompt("Enter third number")  // Create a Scanner object
+var input4 = prompt("Enter fourth number")  // Create a Scanner object
+
+var num1 = Number(input1)
+var num2 = Number(input2)
+var num3 = Number(input3)
+var num4 = Number(input4)
+
+var sum = num1 + num2 + num3 + num4;
+var avg = sum / 4;
+
+alert("average of number input:"+avg)
